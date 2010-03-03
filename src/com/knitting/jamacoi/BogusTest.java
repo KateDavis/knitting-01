0994a773b970b00464f2a02874092a7449fa2b9d
Scott5mith <Scott5mith@aol.com>
2010-03-03 02:02:20 +0000

    still playing

--- src/com/knitting/jamacoi/BogusTest.java
+++ src/com/knitting/jamacoi/BogusTest.java
@@ -9,6 +9,13 @@ public class BogusTest {
 	@Test
 	public void testSomething() {
 		assertTrue(1 == 1);
+		
+		double expected = 7.0;
+		double calculated = 7.00001;
+		
+		assertEquals(expected, calculated, .01);
+		assertEquals(expected, calculated, .000001);
 	}
+
 
 }
