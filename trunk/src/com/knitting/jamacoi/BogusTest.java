642c2ffbcf14f1960906aab6f7d5e91d1e39cc99
Scott5mith@aol.com <Scott5mith@aol.com@687b8e60-1799-11df-b5b1-cb52af195d9b>
2010-03-03 01:53:19 +0000

    adding bogus test

---
+++ trunk/src/com/knitting/jamacoi/BogusTest.java
@@ -0,0 +1,14 @@
+package com.knitting.jamacoi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BogusTest {
+	
+	@Test
+	public void testSomething() {
+		assertTrue(1 == 1);
+	}
+
+}
