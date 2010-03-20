53af46fdb889fc42e4335943e9de82fcbd7a87aa
Scott5mith@aol.com <Scott5mith@aol.com@687b8e60-1799-11df-b5b1-cb52af195d9b>
2010-03-20 03:26:48 +0000

    reformatted to highlight logic

--- trunk/src_test/com/knitting/datasource/DataSetTest.java
+++ trunk/src_test/com/knitting/datasource/DataSetTest.java
@@ -2,17 +2,32 @@ package com.knitting.datasource;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class DataSetTest {
 	
+	@Before
+	public void before() {
+		System.err.println("before test");
+	}
+	
+	@After
+	public void after() {
+		System.err.println("after  test");
+	}
+	
 	@Test
 	public void testInitialization() {
-		DataSet set = new DataSet();
-		assertEquals(0, set.size());
-		DataSeries series = new DataSeries();
+		DataSet           set    = new DataSet   ();
+		assertEquals(0,   set    . size());
+		
+		DataSeries        series = new DataSeries();
+		
 		set.addDataSeries(series);
 		assertEquals(1, set.size());
+		
 		set.addDataSeries(series);
 		assertEquals(1, set.size());
 	}
