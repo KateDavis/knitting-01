c373b63e525f9b755853124adb47106ad333743a
Scott5mith <Scott5mith@aol.com>
2010-03-21 22:26:18 +0000

    Still need to complete this test

---
+++ src_test/com/knitting/jamacoi/Test_Report_Significant_Analysis.java
@@ -0,0 +1,24 @@
+package com.knitting.jamacoi;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Test_Report_Significant_Analysis {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGet() {
+		fail("Not yet implemented");
+	}
+
+}
