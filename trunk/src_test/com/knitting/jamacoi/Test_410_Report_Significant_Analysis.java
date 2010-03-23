274a6937e84a1932f9e085370ad8930c96150bf9
Scott5mith@aol.com <Scott5mith@aol.com@687b8e60-1799-11df-b5b1-cb52af195d9b>
2010-03-23 20:50:27 +0000

    separated different states into different tests.

---
+++ trunk/src_test/com/knitting/jamacoi/Test_410_Report_Significant_Analysis.java
@@ -0,0 +1,79 @@
+package com.knitting.jamacoi;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class   Test_410_Report_Significant_Analysis {
+static private boolean     concatentate_true    = true ;	
+static private boolean     concatentate_false   = false;
+
+static private String      file_name_expected   = "C:/Temp/expected";
+
+static private String      file_name_actual     = "C:/Temp/actual"	
+	                                            + "/"
+	                                            + "Test_410_mixed_significant.txt";
+static private FileWriter  Rpt       ;
+	   private FileWriter  Rpt_Detail;
+	   
+    @BeforeClass
+    public static void setUpBeforeClass() 
+           throws Exception            {
+    	   Rpt  = new FileWriter       ( file_name_actual
+                                       , concatentate_false
+                                       );
+    	   Rpt  . close();
+		}
+
+	@Before
+	public void   setUp     () 
+	       throws Exception {
+		   Rpt_Detail  = new FileWriter( file_name_actual
+                                       , concatentate_true
+                                       );
+	}
+
+	@After
+	public void   tearDown   () 
+	       throws Exception  {
+		   Rpt_Detail . close();
+	}
+
+	@Test
+	public void   test_410_mixed_significant_Get() {
+		   int    r_d_of_freedom             =  3  ;
+		   int    y_d_of_freedom             = 10  ;
+		   double f_value                    = 15.0;
+		   Report_Significant_Analysis  rsa  =  new
+		   Report_Significant_Analysis       (  Rpt_Detail
+				                             ,  r_d_of_freedom
+				                             ,  y_d_of_freedom
+				                             ,  f_value
+				                             )  ;
+		   try   {
+			      rsa.get();
+		         } 
+		   catch (not_estimated   e) {
+		          // TODO Auto-generated catch block
+			      e.printStackTrace();
+                 } 
+		   catch (not_invertable  e) {
+			      // TODO Auto-generated catch block
+			      e.printStackTrace();
+                 } 
+		   catch (not_significant e) {
+			      // TODO Auto-generated catch block
+			      e.printStackTrace();
+                 } 
+		   catch (IOException e    ) {
+			      // TODO Auto-generated catch block
+			      e.printStackTrace();
+		         };
+	}
+
+
+}
