8b8c2e03064911acd08ef228fdf418a4c3a43e8e
Scott5mith@aol.com <Scott5mith@aol.com@687b8e60-1799-11df-b5b1-cb52af195d9b>
2010-03-27 23:23:22 +0000

    Initial coding of this class.  It's not complete.

---
+++ trunk/src/com/knitting/jamacoi/Compare_Two_Files.java
@@ -0,0 +1,61 @@
+package com.knitting.jamacoi;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public  class   Compare_Two_Files {
+	    private String                   File_Name_Expect_Contents;
+        private String                   File_Name_Actual_Contents;
+	   
+private Compare_Two_Files       (){};    // to prevent its use.
+public  Compare_Two_Files       ( String File_Name_Expect_Contents
+		                        , String File_Name_Actual_Contents
+		                        ) {
+	    this.File_Name_Expect_Contents = File_Name_Expect_Contents;
+	    this.File_Name_Actual_Contents = File_Name_Actual_Contents;	
+}
+public  boolean exists_Expected(){	
+	    return  exists_File    ( File_Name_Expect_Contents);
+	
+}
+public  boolean exists_Actual  (){	
+        return  exists_File    ( File_Name_Actual_Contents);
+
+}
+private boolean exists_File    ( String                    File_Name ){
+	    InputStream   stream  =  this.getClass()
+                                     .getResourceAsStream( File_Name );
+	    return        stream ==  null;
+}
+public  boolean equal_Files(){
+        ArrayList    <String> expected = load_List( File_Name_Expect_Contents );
+        ArrayList    <String> actual   = load_List( File_Name_Actual_Contents );
+
+        return false;
+}
+private ArrayList<String>     load_List ( String                   File_Name){
+	    ArrayList<String>          list = new ArrayList<String>();
+try    {	    
+	    InputStream              stream = this.getClass()
+                                              .getResourceAsStream(File_Name);
+
+        Scanner                 scanner = new Scanner( stream );
+        while (                 scanner . hasNextLine()
+              )
+              { 
+                list.add(       scanner . nextLine   () );
+              }
+        stream.close();
+       }
+catch  (Exception e)
+       {System.out.println( "ERROR: Input file = >"
+    		              +  File_Name
+    		              + "< does not exist or cannot be found."
+    		              );
+       }
+		return  list;
+}
+	   
+
+}
