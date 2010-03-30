a8a2b8c19f7b598a8b7c8ab5a3831cbb896dacb3
Scott5mith@aol.com <Scott5mith@aol.com@687b8e60-1799-11df-b5b1-cb52af195d9b>
2010-03-30 04:22:06 +0000

    added some more logic to this class

--- trunk/src/com/knitting/jamacoi/Compare_Two_ArrayLists.java
+++ trunk/src/com/knitting/jamacoi/Compare_Two_ArrayLists.java
@@ -27,17 +27,85 @@ return  Math.min( expected . size()
 }
 private ArrayList<String> find_First_Difference(){
 	    ArrayList<String> difference_entries = new ArrayList<String>();
-	    int               entry_min          =     get_Min();
-	    int               ix                 =     0;
-	    while ( expected.get(ix) == actual.get(ix)
+	    int                    ix            =     0;
+	    while ( (              ix   < get_Min()      )
+	    		&&
+	    		( expected.get(ix) == actual.get(ix) )
 	    	  )  
 	          {
-	    	               ++ix;
+	    	                 ++ix;
 	          }
+	    if    ( ix < get_Min() )
+	          {
+	    	    get_non_matching_lines( ix
+	    	    		              , difference_entries
+	    	    		              );
+	          }
+	    if    ( get_Min() < expected.size() )
+              {
+                get_unmatched_line    ( ix
+                		              , difference_entries
+                		              );
+              }
+	    if    ( get_Min() < actual  .size() )
+              {
+                get_extra_line        ( ix
+                		              , difference_entries
+                		              );
+              }
 	    
-return  difference_entries;	    
-	
+return  difference_entries;	
 }
 
+private void   get_non_matching_lines( int                ix
+		                             , ArrayList<String>  difference_entries
+		                             ) {
+        difference_entries.add( get_row_expected(ix) );
+        difference_entries.add( get_row_actual  (ix) );
+}
+private void   get_unmatched_line    ( int                ix
+		                             , ArrayList<String>  difference_entries
+		                             ) {
+        difference_entries.add( get_row_expected(ix) );
+        difference_entries.add( get_row_missing (ix) );	
+}
+private void   get_extra_line        ( int                ix
+                                     , ArrayList<String>  difference_entries
+                                     ) {
+	    difference_entries.add( get_row_actual  (ix) );
+	    difference_entries.add( get_row_extra   (ix) );
+}
+private String get_row_expected ( int ix ){
+        String e = "row "
+                 +  ix
+                 + ": expected =>"
+                 +    expected.get(ix)
+                 + "<";
+return         e;
+}
+private String get_row_actual   ( int ix ){
+        String a = "row "
+                 +  ix
+                 + ": actual   =>"
+                 +    actual.get(ix)
+                 + "<";
+return         a;
+}
+private String get_row_missing  ( int ix ){
+        String a = "row "
+                 +  ix
+                 + ": actual   =>"
+                 + "Error: there is no corresponding line here"
+                 + "<";
+return         a;
+}
+private String get_row_extra    ( int ix ){
+        String a = "row "
+                 +  ix
+                 + ": actual   =>"
+                 + "Error: this is an extra line that should not be here"
+                 + "<";
+return         a;
+}
 
 }
