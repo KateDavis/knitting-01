a0488c331e7d5a084fb5e6a4b951a1354fef6691
aarondcox@yahoo.com <aarondcox@yahoo.com@687b8e60-1799-11df-b5b1-cb52af195d9b>
2010-04-03 04:46:15 +0000

    Flush out the DataSet/DataSeries classes

--- trunk/src/com/knitting/datasource/DataSeries.java
+++ trunk/src/com/knitting/datasource/DataSeries.java
@@ -1,5 +1,37 @@
 package com.knitting.datasource;
 
-public class DataSeries {
+import java.util.Date;
+import java.util.List;
+
+/**
+ * The definition of a DataSeries where each 'point' contains one domain coordinate that is associated with a date,
+ * and multiple range values that represent the y coordinates for that domain point.
+ *
+ */
+public interface DataSeries {
+
+	/**
+	 * Sets the Y values for the date domain coordinate.  If the domain coordinate already exists, its values are
+	 * replaced with these.
+	 * 
+	 * @param date that defines the domain coordinate
+	 * @param values to add as range points for the domain
+	 */
+	void setValue(Date date, Double... values);
+
+	/**
+	 * Returns a list of range values for the given index.
+	 * 
+	 * @param index for the range values to retrieve
+	 * @return a list of range values for the given index
+	 */
+	List<Double> getValues(int index);
+
+	/**
+	 * Returns the DataSet that this DataSeries is contained in.
+	 * 
+	 * @return the DataSet that this DataSeries is contained in
+	 */
+	DataSet getDataset();
 
 }
