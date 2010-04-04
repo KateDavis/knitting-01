eed006690df9c784560dc9aef10d3c7cb5a5b56a
Scott5mith <Scott5mith@aol.com>
2010-04-04 22:44:10 +0000

    Contains coding errors.  Will correct later.

--- src/com/knitting/jamacoi/Compare_Two_Files.java
+++ src/com/knitting/jamacoi/Compare_Two_Files.java
@@ -1,26 +1,34 @@
 package com.knitting.jamacoi;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public  class   Compare_Two_Files {
-	    private String                   File_Name_Expect_Contents;
-        private String                   File_Name_Actual_Contents;
+	    private String                   File_Name_Contents_Expected;
+        private String                   File_Name_Contents_Actual;
+        private String                   File_Name_Contents_Difference;
+        private String                   Dir_Name_Actual = "actual";
 	   
 private Compare_Two_Files       (){};    // to prevent its use.
-public  Compare_Two_Files       ( String File_Name_Expect_Contents
-		                        , String File_Name_Actual_Contents
+public  Compare_Two_Files       ( String File_Name_Contents_Expected
+		                        , String File_Name_Contents_Actual
+		                        , String File_Name_Contents_Difference
 		                        ) {
-	    this.File_Name_Expect_Contents = File_Name_Expect_Contents;
-	    this.File_Name_Actual_Contents = File_Name_Actual_Contents;	
+	    this.File_Name_Contents_Expected   = File_Name_Contents_Expected;
+	    this.File_Name_Contents_Actual     = File_Name_Contents_Actual;
+	    this.File_Name_Contents_Difference = File_Name_Contents_Difference;
 }
 public  boolean exists_Expected(){	
-	    return  exists_File    (         File_Name_Expect_Contents);
+	    return  exists_File    (             File_Name_Contents_Expected);
 	
 }
 public  boolean exists_Actual  (){	
-        return  exists_File    (         File_Name_Actual_Contents);
+        return  exists_File    (             File_Name_Contents_Actual);
 
 }
 private boolean exists_File    ( String                    File_Name ){
@@ -29,14 +37,23 @@ private boolean exists_File    ( String                    File_Name ){
 	    return        stream ==  null;
 }
 public  boolean equal_Files(){
-        ArrayList    <String> expected = load_List( File_Name_Expect_Contents );
-        ArrayList    <String> actual   = load_List( File_Name_Actual_Contents );
+        ArrayList    <String> expected = load_List( File_Name_Contents_Expected );
+        ArrayList    <String> actual   = load_List( File_Name_Contents_Actual   );
         
         Compare_Two_ArrayLists diff    = new Compare_Two_ArrayLists ( expected
                                                                     , actual
                                                                     );
-
-        return diff.are_Equal();
+        boolean d                      = diff.are_Equal();
+        
+        if  ( d )
+            {
+        	  delete_diff_file();
+            }
+        else
+            {
+        	  create_diff_file( diff );
+            }
+        return d;
 }
 private ArrayList<String>     load_List ( String                   File_Name){
 	    ArrayList<String>          list = new ArrayList<String>();
@@ -60,6 +77,27 @@ catch  (Exception e)
        }
 		return  list;
 }
-	   
-
+public String get_Dir_Actaul(){
+	   URL    url_Name = this . getClass() . getResource(Dir_Name_Actual);
+return      ( url_Name        . getPath () );	
+}
+protected void delete_diff_file(){
+	      File diff = new File (File_Name_Contents_Difference);
+	      if ( diff.exists() )
+	         {
+               diff.delete();
+             }
+}
+protected void create_diff_file( ArrayList<String> diff)
+          throws IOException
+{
+	      delete_diff_file();
+try      {	      
+	      FileWriter d = new FileWriter( File_Name_Contents_Difference);
+         }
+catch    (IOException e)
+         { throw      e;
+         }
+         }
+	      
 }
