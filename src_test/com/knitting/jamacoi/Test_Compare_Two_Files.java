ce8ce6f606fda569653eae32e16b893f127a14ff
Scott5mith <Scott5mith@aol.com>
2010-04-06 04:05:36 +0000

    added a fix

--- src_test/com/knitting/jamacoi/Test_Compare_Two_Files.java
+++ src_test/com/knitting/jamacoi/Test_Compare_Two_Files.java
@@ -11,12 +11,14 @@ import org.junit.Test;
 
 public class Test_Compare_Two_Files {
 	
-static String  File_Name      = "Test_400_not_significant.txt";
-static String  Dir__Actual    = "actual";
-static String  Dir__Expected  = "expected";
-static String  File_Separator = File.pathSeparator;
-String  Dir__Actual_Full;
+static String  File_Name       = "Test_400_not_significant.txt";
+static String  Dir__Actual     = "actual";
+static String  Dir__Expected   = "expected";
+static String  Dir__Difference = "difference";
+static String  File_Separator  = File.pathSeparator;
+       String  Dir__Actual_Full;
        String  Dir__Expected_Full;
+       String  Dir__Difference_Full;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -28,13 +30,17 @@ String  Dir__Actual_Full;
 
 	@Before
 	public void setUp() throws Exception {
-           URL  actual        = this     . getClass()
-                                         . getResource(Dir__Actual);
-           Dir__Actual_Full   = actual   . getPath(); 
+           URL  actual          = this     . getClass()
+                                           . getResource(Dir__Actual    );
+           Dir__Actual_Full     = actual   . getPath(); 
            
-           URL  expected      = this     . getClass()
-                                         . getResource(Dir__Expected);
-           Dir__Expected_Full = expected . getPath();          
+           URL  expected        = this     . getClass()
+                                           . getResource(Dir__Expected  );
+           Dir__Expected_Full   = expected . getPath();
+           
+           URL  difference      = this     . getClass()
+                                           . getResource(Dir__Difference);
+           Dir__Difference_Full = expected . getPath();  
 	}
 
 	@After
@@ -52,16 +58,22 @@ String  Dir__Actual_Full;
                           +  Dir__Expected_Full
                           + "<"
                           ) ;
-		String Full_File_Actual    = Dir__Actual_Full
-		                           + File.pathSeparator
-		                           + File_Name;
+		String Full_File_Actual     = Dir__Actual_Full
+		                            + File.pathSeparator
+		                            + File_Name;
 		
-		String Full_File_Expected  = Dir__Expected_Full
-                                   + File.pathSeparator
-                                   + File_Name;
-		Compare_Two_Files c        = new Compare_Two_Files( Full_File_Expected
-				                                          , Full_File_Actual
-				                                          ) ;
+		String Full_File_Expected   = Dir__Expected_Full
+                                    + File.pathSeparator
+                                    + File_Name;
+		
+		String Full_File_Difference = Dir__Difference_Full
+                                    + File.pathSeparator
+                                    + File_Name;
+		
+		Compare_Two_Files c         = new Compare_Two_Files( Full_File_Expected
+				                                           , Full_File_Actual
+				                                           , Full_File_Difference
+				                                           ) ;
 		System.out.println( "Dir__Actual_Full for c = >"
 			              +  c.get_Dir_Actaul()
 			              + "<"
