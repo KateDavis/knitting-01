875453979e2f0238fc9744405013622f0dd68c7d
Scott5mith@aol.com <Scott5mith@aol.com@687b8e60-1799-11df-b5b1-cb52af195d9b>
2010-04-11 22:21:14 +0000

    added file.exists() tests.

--- trunk/src_test/com/knitting/jamacoi/Test_Compare_Two_Files.java
+++ trunk/src_test/com/knitting/jamacoi/Test_Compare_Two_Files.java
@@ -1,7 +1,9 @@
 package com.knitting.jamacoi;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
 import java.net.URL;
 
 import org.junit.After;
@@ -160,15 +162,23 @@ static String  Dir__Difference      = "difference";
 		  show_Full_File_Expected   ();
 		  show_Full_File_Actual     ();
 		  show_Full_File_Difference ();
+		  
+		  File e = new File    ( Full_File_Expected   );
+		  File a = new File    ( Full_File_Actual     );
+		  File d = new File    ( Full_File_Difference );
+		  
+		  assertTrue  ( e.exists() );
+		  assertTrue  ( a.exists() );
+		  assertFalse ( d.exists() );
 		
-          c                         =  new  Compare_Two_Files 
-                                            (  Full_File_Expected
-                                            ,  Full_File_Actual
-                                            ,  Full_File_Difference
-                                            )  ;
-		  assertTrue ( c.exists_Expected() );
-		  assertTrue ( c.exists_Actual  () );
-		  assertTrue ( c.equal_Files    () );
+//          c                         =  new  Compare_Two_Files 
+//                                            (  Full_File_Expected
+//                                            ,  Full_File_Actual
+//                                            ,  Full_File_Difference
+//                                            )  ;
+//		  assertTrue ( c.exists_Expected() );
+//		  assertTrue ( c.exists_Actual  () );
+//		  assertTrue ( c.equal_Files    () );
 	}
 
 }
