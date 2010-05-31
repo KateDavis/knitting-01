85f29ddc4731e65a8dfdbe486def9250b829ce77
Scott5mith@aol.com <Scott5mith@aol.com@687b8e60-1799-11df-b5b1-cb52af195d9b>
2010-05-31 21:02:57 +0000

    to synch recent work.

--- trunk/src/com/knitting/jamacoi/Request_Series.java
+++ trunk/src/com/knitting/jamacoi/Request_Series.java
@@ -1,25 +1,25 @@
 package com.knitting.jamacoi;
 
 import java.net.URI;
-import java.util.SortedSet;
+import java.util.Comparator;
+import java.util.TreeSet;
 
-public  class      Request_Series{ 
+public  class      Request_Series
+        implements Comparator<Request_Series>{ 
 
 		private    URI                     data_location;
 	    private    String                  human_name;
-	    protected  SortedSet<Integer>      lags;
+	    protected  TreeSet<Integer>        lags;
 private            Request_Series(){}; //  prevent this constructor
 public             Request_Series( URI     data_location
 		                         , String  human_name
 		                         ) {
 	    this.data_location  =              data_location;
 	    this.human_name     =              human_name;
+	    this.lags           =          new TreeSet<Integer>();
 }
 public  URI      get_data_location() {
 	    return       data_location;
-}
-public  void     set_data_location(URI     data_location) {
-	    this .       data_location  =      data_location;
 }
 public  String   get_human_name() {
 	    return       human_name;
@@ -27,11 +27,23 @@ public  String   get_human_name() {
 public  void     set_human_name(String     human_name) {
 	    this .       human_name     =      human_name;
 }
-public  SortedSet<Integer>  get_lags(){
-	    return                  lags;
+public  TreeSet<Integer>  get_lags(){
+	    return                lags;
 }
-public  void     set_lags(SortedSet<Integer>  lags){
-        this .       lags           =         lags;
+public  void     set_lags(TreeSet<Integer> lags){
+        this .       lags           =      lags;
+}
+public  int      compare(Request_Series rs_0
+		                ,Request_Series rs_1
+		                ){
+	    String   s0 = rs_0.get_data_location().toString();
+	    String   s1 = rs_1.get_data_location().toString();
+	    return   s0 . compareTo(s1);	
+}
+public  boolean  equals (Request_Series rs){
+        String   s0 =        data_location  .toString();
+        String   s1 = rs.get_data_location().toString();
+        return   s0 == s1;
 }
 }
 
