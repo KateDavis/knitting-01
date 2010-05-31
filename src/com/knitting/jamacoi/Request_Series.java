e5a5eaa2f9944197884866f158f8f27d8c4da212
Scott5mith <Scott5mith@aol.com>
2010-05-31 22:13:24 +0000

    added equals(Object obj)

--- src/com/knitting/jamacoi/Request_Series.java
+++ src/com/knitting/jamacoi/Request_Series.java
@@ -1,13 +1,12 @@
 package com.knitting.jamacoi;
 
 import java.net.URI;
-import java.util.Comparator;
 import java.util.TreeSet;
 
-public  class      Request_Series
-        implements Comparator<Request_Series>{ 
+final   public     class      Request_Series{
+//        implements Comparable<Request_Series>{ 
 
-		private    URI                     data_location;
+    	private    URI                     data_location;
 	    private    String                  human_name;
 	    protected  TreeSet<Integer>        lags;
 private            Request_Series(){}; //  prevent this constructor
@@ -33,17 +32,16 @@ public  TreeSet<Integer>  get_lags(){
 public  void     set_lags(TreeSet<Integer> lags){
         this .       lags           =      lags;
 }
-public  int      compare(Request_Series rs_0
-		                ,Request_Series rs_1
-		                ){
-	    String   s0 = rs_0.get_data_location().toString();
-	    String   s1 = rs_1.get_data_location().toString();
-	    return   s0 . compareTo(s1);	
+public  boolean  equals (Object obj){
+	    if (     this    ==     obj           )   return true ;
+	    if (  !( obj instanceof Request_Series) ) return false;
+	    String   s0 =    this                 .get_data_location().toString();
+	    String   s1 = ( (Request_Series)obj ) .get_data_location().toString();
+	    return ( s0.equals(s1) );
 }
-public  boolean  equals (Request_Series rs){
-        String   s0 =        data_location  .toString();
-        String   s1 = rs.get_data_location().toString();
-        return   s0 == s1;
+public  int      hashcode(){
+	    String   s0 = this.get_data_location().toString();
+	    return   s0.hashCode();
 }
 }
 
