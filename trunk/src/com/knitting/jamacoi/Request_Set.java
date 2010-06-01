64a59148deabac7056e51462e75e5bd93436c2dd
Scott5mith@aol.com <Scott5mith@aol.com@687b8e60-1799-11df-b5b1-cb52af195d9b>
2010-06-01 02:53:59 +0000

    initial coding.

---
+++ trunk/src/com/knitting/jamacoi/Request_Set.java
@@ -0,0 +1,11 @@
+package com.knitting.jamacoi;
+
+import java.util.LinkedHashSet;
+
+public class     Request_Set {
+
+	   protected LinkedHashSet<Request_Series> instruments = new LinkedHashSet<Request_Series>();
+	   
+public           Request_Set(){};
+	
+}
