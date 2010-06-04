a751dc02cccca3519318eaf97b706a1772e686ac
Scott5mith <Scott5mith@aol.com>
2010-06-04 17:53:30 +0000

    changed method name

--- src/com/knitting/util/K_String.java
+++ src/com/knitting/util/K_String.java
@@ -1,10 +1,15 @@
 package com.knitting.util;
 
 public class  K_String {
-public static String     get_file_name_short(final String s){
+public static String     get_file_name_short   (final String s){
 	   return s.substring( s.lastIndexOf('/')
 			             + 1
-			             );
+			             ) ;
 }
+public static String     remove_file_extention (final String s){
+	   return s.substring( 0
+			             , s.lastIndexOf('.')
+	                     ) ;
+}	   
 }
 
