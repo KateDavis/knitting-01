108e0950aad8ec6bf3d9634807e564364dfc1145
Scott5mith <Scott5mith@aol.com>
2010-06-05 03:59:35 +0000

    corrected method logic.

--- src/com/knitting/util/K_String.java
+++ src/com/knitting/util/K_String.java
@@ -1,15 +1,16 @@
 package com.knitting.util;
 
 public class  K_String {
-public static String     get_file_name_short   (final String s){
+public static String       get_file_name_short   (final String s){
 	   return s.substring( s.lastIndexOf('/')
 			             + 1
 			             ) ;
 }
-public static String     remove_file_extention (final String s){
+public static String       remove_file_extention (final String s){
 	   return s.substring( 0
-			             , s.lastIndexOf('.')
-	                     ) ;
+			             ,(s.lastIndexOf('.') == -1) ? s.length()
+			                	                     : s.lastIndexOf('.')
+			             ) ;	                     
 }	   
 }
 
