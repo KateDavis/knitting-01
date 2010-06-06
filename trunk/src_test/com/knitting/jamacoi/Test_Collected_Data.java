93ede575afee4a89271f524487197249dcc6c56a
Scott5mith@aol.com <Scott5mith@aol.com@687b8e60-1799-11df-b5b1-cb52af195d9b>
2010-06-06 22:28:57 +0000

    initial logic load.

---
+++ trunk/src_test/com/knitting/jamacoi/Test_Collected_Data.java
@@ -0,0 +1,43 @@
+package com.knitting.jamacoi;
+
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Test_Collected_Data {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	@Test
+	public void test_01_new_Collected_Data(){
+		   Collected_Data c_data   = new Collected_Data();
+		   
+		   String         earliest = c_data.get_fmt_shared_start__earliest();
+		   String         lastest  = c_data.get_fmt_shared_ending_lastest ();
+		   Integer        max      = c_data.get_lag_max();
+		   
+		   assertTrue   ( earliest.equals("3000_12_31") );
+		   assertTrue   ( lastest .equals("1900_01_01") );
+		   assertTrue   ( max      ==      0            );
+		   
+	}
+
+}
