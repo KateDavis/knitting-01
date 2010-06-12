63d2fef25a87e02824da3bacb28c78300dd349ad
Scott5mith@aol.com <Scott5mith@aol.com@687b8e60-1799-11df-b5b1-cb52af195d9b>
2010-06-12 20:17:01 +0000

    added new tests

--- trunk/src_test/com/knitting/jamacoi/Test_Collected_Data.java
+++ trunk/src_test/com/knitting/jamacoi/Test_Collected_Data.java
@@ -1,7 +1,12 @@
 package com.knitting.jamacoi;
 
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+
+import java.net.URI;
+import java.net.URL;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -10,9 +15,24 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class Test_Collected_Data {
+	static String          Dir__Prefix   = "knitting-01/src_test/com/knitting/datasource";
+	static String          AAPL          = Dir__Prefix + "/AAPL.txt";
+
+	
+           String          name;
+           URL             url_this;
+           URL             url_workspace;
+           URL             url_AAPL;
+           
+           URI             uri_AAPL;
+           URI             uri_AMZN;
+           URI             uri_QCOM;
+           URI             uri_GLD;
+           
+           Request_Series  rs__AAPL;
 
 	@BeforeClass
-	public static void setUpBeforeClass()   throws Exception {
+	public static void setUpBeforeClass() throws Exception {
 	}
 
 	@AfterClass
@@ -21,23 +41,91 @@ public class Test_Collected_Data {
 
 	@Before
 	public void setUp() throws Exception {
+                name           = this.getClass().getSimpleName();
+	    
+                url_this       = this.getClass   ()
+                                     .getResource( name + ".class" );
+assertNotNull ( url_this );
+
+                url_workspace  = new URL( url_this     , "../../../../../");
+assertNotNull ( url_workspace  ) ;
+
+                url_AAPL       = new URL( url_workspace, AAPL );
+assertNotNull ( url_AAPL       ) ;
+
+
+                uri_AAPL       =          url_AAPL.toURI();
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
 	@Test
-	public void test_01_new_Collected_Data(){
-		   Collected_Data c_data   = new Collected_Data();
-		   
-		   String         earliest = c_data.get_fmt_shared_start__earliest();
-		   String         lastest  = c_data.get_fmt_shared_ending_lastest ();
-		   Integer        max      = c_data.get_lag_max();
-		   
-		   assertTrue   ( earliest.equals("1900_01_01") );
-		   assertTrue   ( lastest .equals("3000_12_31") );
-		   assertTrue   ( max      ==      0            );
-		   
+	public void test_01_valid_URI(){
+		   Collected_Data c  = new Collected_Data("AAPL"
+				                                 , uri_AAPL
+				                                 ) ;
+		   System.out.println( "State       = >"
+				             +  c.get_state_desc()
+				             + "<" 
+				             ) ;
+		   System.out.println( "date__count = >"
+				             +  c.get_date_count()
+				             + "<"
+				             );
+		   assertFalse       ( c.is_data_empty ()         );
+		   assertTrue        ( c.get_date_count() == 1353 );
 	}
-
+	@Test
+	public void test_02_get_first_date(){
+		 Collected_Data c   =  new Collected_Data("AAPL"
+                                                 , uri_AAPL
+                                                 ) ;
+		 String date__first =  c.get_first_date();
+		 System.out.println ( "date__first = >"
+				            +  date__first
+				            + "<"
+				            ); 
+		 String s           = "2004_11_25";
+		 assertTrue         (  date__first.equals( s ) );
+	}
+	@Test
+	public void test_03_get_first_price(){
+		 Collected_Data c   =  new Collected_Data("AAPL"
+                                                 , uri_AAPL
+                                                 ) ;
+		 Double price_first =  c.get_first_price();
+		 System.out.println ( "price_first = >"
+				            +  price_first
+				            + "<"
+				            );
+		 assertTrue         (  price_first  ==  23.775 );
+		                   ++  price_first;
+		 assertTrue         (  price_first  ==  24.775 );                   
+	}
+	@Test
+	public void test_04_get_last__date(){
+		 Collected_Data c  =  new Collected_Data("AAPL"
+                                                , uri_AAPL
+                                                ) ;
+		 String date__last =  c.get_last__date();
+		 System.out.println( "date__last  = >"
+				           +  date__last
+				           + "<"
+				           );
+		 String s          = "2010_05_09";
+		 assertTrue        (  date__last.equals(s) ); 
+	}
+	@Test
+	public void test_05_get_last__price(){
+		 Collected_Data c  = new Collected_Data("AAPL"
+                                               , uri_AAPL
+                                               ) ;
+		 Double price_last = c.get_last__price();
+		 System.out.println( "price_last  = >"
+				           +  price_last
+				           + "<"
+				           );
+		 assertTrue        (  price_last  ==  241.7899932861328 ); 
+	}
 }
