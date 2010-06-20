64f0781a662bac9173bfd2dec1376355a6c8cec7
Scott5mith <Scott5mith@aol.com>
2010-06-20 17:41:32 +0000

    initial logic load

---
+++ src/com/knitting/jamacoi/lagged_matrix.java
@@ -0,0 +1,11 @@
+package com.knitting.jamacoi;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.TreeMap;
+
+public class    lagged_matrix {
+   
+	   private  int                                  row_size;
+	   private  TreeMap  <Date, ArrayList<Double> >  matrix  = new TreeMap  <Date, ArrayList<Double> >  ()  ;
+}
