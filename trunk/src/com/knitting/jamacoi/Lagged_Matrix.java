9ee830c565c4ff0de1cdaa8e859832b3c966a304
Scott5mith@aol.com <Scott5mith@aol.com@687b8e60-1799-11df-b5b1-cb52af195d9b>
2010-06-20 19:04:00 +0000

    added some formatting options.

--- trunk/src/com/knitting/jamacoi/Lagged_Matrix.java
+++ trunk/src/com/knitting/jamacoi/Lagged_Matrix.java
@@ -1,7 +1,12 @@
 package com.knitting.jamacoi;
 
 import java.util.Date;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
+
+import com.knitting.util.K_Calendar;
 
 public    class      Lagged_Matrix {
           private    int                      price_cols;
@@ -24,5 +29,52 @@ public    void       put( final Date       date
 		                , final Price_Row  pr
 		                ) {
 	      tm.put(date, pr);
+}
+public    void       print_ix_entries(final int ix_max){
+	
+	      Set     <Map.Entry<Date,Price_Row>> set  = tm .entrySet();
+	      Iterator<Map.Entry<Date,Price_Row>> iter = set.iterator();
+	      int                                 ix   = 0;
+	      
+	      while (
+	    		   ( iter.hasNext()   )
+	    		   &&
+	    		   ( ix < ix_max      )
+	    		)
+	            {  System.out.println(fmt_entry_space_decimal( iter.next() 
+	            		                                     )
+	            		             );
+	    	       ++ix;
+	            }
+}
+protected String     fmt_entry_space_decimal(final Map.Entry<Date,Price_Row>  me ){
+	      K_Calendar kcal = new K_Calendar();
+	      String     date =     kcal.get_ccyy_mm_dd( (  me.getKey  () ) );
+	      Price_Row  pr   =                             me.getValue();
+	      String     row  =     pr.fmt_row_space_and_decimal();
+	      return     (  date
+	    		     + ","
+	    		     +  row
+	    		     ) ;
+}
+protected String     fmt_entry_comma_decimal(final Map.Entry<Date,Price_Row>  me ){
+          K_Calendar kcal = new K_Calendar();
+          String     date =     kcal.get_ccyy_mm_dd( (  me.getKey  () ) );
+          Price_Row  pr   =                             me.getValue();
+          String     row  =     pr.fmt_row_comma_and_decimal();
+          return     (  date
+  		             + ","
+  		             +  row
+  		             ) ;
+}
+protected String     fmt_entry_comma_scientific(final Map.Entry<Date,Price_Row>  me ){
+          K_Calendar kcal = new K_Calendar();
+          String     date =     kcal.get_ccyy_mm_dd( (  me.getKey  () ) );
+          Price_Row  pr   =                             me.getValue();
+          String     row  =     pr.fmt_row_comma_and_scientific();
+          return     (  date
+	                 + ","
+	                 +  row
+	                 ) ;
 }
 }
