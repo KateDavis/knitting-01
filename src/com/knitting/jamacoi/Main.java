b7d29442b6a494ff35ed1e347b1748b055468fc1
Scott5mith <Scott5mith@aol.com>
2010-07-18 21:30:20 +0000

    Replaced detailed code to read xml file with a Workspace class so Main can determine the Eclipe workspace.

--- src/com/knitting/jamacoi/Main.java
+++ src/com/knitting/jamacoi/Main.java
@@ -1,24 +1,35 @@
 package com.knitting.jamacoi;
+import static org.junit.Assert.assertNotNull;
+
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 import com.thoughtworks.xstream.XStream;
 
 public class Main 
 {
-	public static void  main(String[] args)
-    throws       IOException
-         ,       not_estimated
-         ,       not_invertable
-         ,       not_rectangular
-         ,       not_significant
+	static String  Dir__Config           = "knitting-01/src_resources/com/knitting/resources/configuration";
+	static String          xml           =  Dir__Config    + "/test_xml_02.xml";
+	
+	public static  void  main(String[] args)
+    throws         IOException
+         ,         not_estimated
+         ,         not_invertable
+         ,         not_rectangular
+         ,         not_significant
 {
-String             file_xml_name_02 =   "/Temp/test_xml_02.xml";
+Workspace          ws            = new Workspace();	
+URL                url_workspace = ws.get_Workspace();
+assertNotNull    ( url_workspace  ) ;
+
+URL                url_xml       = new URL( url_workspace, xml  );
+assertNotNull    ( url_xml        ) ;
     
-Build_xml_string   xml_in      = new Build_xml_string   ( file_xml_name_02 );
+Build_xml_string   xml_in      = new Build_xml_string   ( url_xml );
 XStream            xstream     = new XStream();
 Analysis_Parms     my_parms_02 = (Analysis_Parms)
-                                xstream.fromXML( xml_in.get_xml_string() );
+                                  xstream.fromXML( xml_in.get_xml_string() );
 
                    my_parms_02 . report_values();
      
