4adaebf54247ee7f485a33bf26ac61bffa31da2d
Scott5mith <Scott5mith@aol.com>
2010-07-18 18:52:01 +0000

    Added new constructor that takes a URL parm instead of a String parm.

--- src/com/knitting/jamacoi/Build_xml_string.java
+++ src/com/knitting/jamacoi/Build_xml_string.java
@@ -1,6 +1,11 @@
 package com.knitting.jamacoi;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.net.URL;
 public class Build_xml_string {
 	private String xml_trimmed;
     
@@ -32,6 +37,42 @@ public class Build_xml_string {
 	                          );
 	       }   
 	}      
+	public         Build_xml_string ( URL u )
+	       throws  java.io.IOException
+	{
+		   assertNotNull       ( u ) ;
+		   
+		   File  f    = new File   ( u.getFile() );
+
+		   assertTrue ( f.exists() );
+		   assertTrue ( f.isFile() );
+		
+		   try   {
+			        FileReader     file_xml = new FileReader    ( f        );
+			        BufferedReader br       = new BufferedReader( file_xml );
+			        String         a_line;
+			        String         a_xml    = "";
+			        while (
+			                 null  != ( a_line = br.readLine() )
+			              )
+			              {
+			                 a_xml  = a_xml
+			                        + a_line
+			                        + "\n";
+			              }
+			        file_xml.close();
+			        xml_trimmed = a_xml.substring(  0
+			                                     , (a_xml.length() -1 )
+			                                     );
+			       } 
+		   catch   (java.io.FileNotFoundException e)
+			       {
+			        System.out.println("ERROR: could not read file refereced in URL "
+			                          + u.toString()
+			                          );
+			       }   
+	}
+	       
 	public String get_xml_string()
 	{
 	       return xml_trimmed;
