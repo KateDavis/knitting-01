851b155460cab051696a7a4bc35a104ef579e946
Scott5mith <Scott5mith@aol.com>
2010-07-18 21:26:17 +0000

    removed a constructor that was not longer referenced.

--- src/com/knitting/jamacoi/Build_xml_string.java
+++ src/com/knitting/jamacoi/Build_xml_string.java
@@ -9,34 +9,6 @@ import java.net.URL;
 public class Build_xml_string {
 	private String xml_trimmed;
     
-	public         Build_xml_string ( String file_name_xml )
-	        throws java.io.IOException
-	{
-	 try   {
-	        FileReader     file_xml = new FileReader    ( file_name_xml);
-	        BufferedReader br       = new BufferedReader( file_xml     );
-	        String         a_line;
-	        String         a_xml    = "";
-	        while (
-	                 null  != ( a_line = br.readLine() )
-	              )
-	              {
-	                 a_xml  = a_xml
-	                        + a_line
-	                        + "\n";
-	              }
-	        file_xml.close();
-	        xml_trimmed = a_xml.substring(  0
-	                                     , (a_xml.length() -1 )
-	                                     );
-	       } 
-	 catch (java.io.FileNotFoundException e)
-	       {
-	        System.out.println("ERROR: could not read "
-	                          + file_name_xml
-	                          );
-	       }   
-	}      
 	public         Build_xml_string ( URL u )
 	       throws  java.io.IOException
 	{
