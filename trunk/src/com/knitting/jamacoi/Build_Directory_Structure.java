871985f59eb8d1004f8c4cf9425a6da0cf8a7a80
Scott5mith@aol.com <Scott5mith@aol.com@687b8e60-1799-11df-b5b1-cb52af195d9b>
2010-08-01 20:50:16 +0000

    corrected constructor logic.

--- trunk/src/com/knitting/jamacoi/Build_Directory_Structure.java
+++ trunk/src/com/knitting/jamacoi/Build_Directory_Structure.java
@@ -5,6 +5,11 @@ import static org.junit.Assert.assertNotNull;
 import java.net.URL;
 
 public   class    Build_Directory_Structure {
+final    private  static  String  reports         =  "reports";
+final    private  static  String  request_series  =  "request_series";
+final    private  static  String  request_set     =  "request_set";
+final    private  static  String  residuals       =  "residuals";
+
          private  URL     url_workspace;
          private  URL     rel_1_application_base;
          private  URL     rel_2_family;
@@ -16,28 +21,17 @@ public   class    Build_Directory_Structure {
          private  String  str_1_application_base;
          private  String  str_2_family;
          private  String  str_3_request;
-         private  String  str_4_reports;
-         private  String  str_4_request_series;
-         private  String  str_4_request_set;
-         private  String  str_4_residuals;
+
 private           Build_Directory_Structure (){;}     // to prevent this constructor
 public            Build_Directory_Structure ( URL     url_workspace
 		                                    , String  str_1_application_base
 		                                    , String  str_2_family
 		                                    , String  str_3_request
-		                                    , String  str_4_reports
-		                                    , String  str_4_request_series
-		                                    , String  str_4_request_set
-		                                    , String  str_4_residuals
 		                                    ){
 	     this.url_workspace                 =         url_workspace;
 	     this.str_1_application_base        =         str_1_application_base;        
 	     this.str_2_family                  =         str_2_family;
 	     this.str_3_request                 =         str_3_request;
-	     this.str_4_reports                 =         str_4_reports;
-	     this.str_4_request_series          =         str_4_request_series;
-	     this.str_4_request_set             =         str_4_request_set;
-	     this.str_4_residuals               =         str_4_residuals;
 }
 public   void    create_urls()
          throws  java.net.MalformedURLException
@@ -72,13 +66,13 @@ StringBuffer     sb4                    = new StringBuffer();
                  sb4.append("/");
 final int        sb4_base_len           = sb4.length();                 
 
-                 sb4.append(str_4_reports);
+                 sb4.append(reports);
                  rel_4_reports          = new URL ( url_workspace,  sb.toString() );
 assertNotNull   (rel_4_reports);
 
                  sb4.replace( sb4_base_len
                 		    , sb4.length()
-                		    , str_4_request_series
+                		    , request_series
                 		    );
                  
                  rel_4_request_series   = new URL ( url_workspace,  sb.toString() );
@@ -86,7 +80,7 @@ assertNotNull   (rel_4_request_series);
 
                  sb4.replace( sb4_base_len
 		                    , sb4.length()
-		                    , str_4_request_set
+		                    , request_set
 		                    );
 
                  rel_4_request_set      = new URL ( url_workspace,  sb.toString() );
@@ -94,7 +88,7 @@ assertNotNull   (rel_4_request_set);
 
                  sb4.replace( sb4_base_len
                             , sb4.length()
-                            , str_4_residuals
+                            , residuals
                             );
 
                  rel_4_residuals        = new URL ( url_workspace,  sb.toString() );
@@ -112,12 +106,9 @@ public void     show_urls(){
 	            System.out.println( " " );
 }
 public void     show_url ( URL  u ){
-	            System.out.print( "url = >"
-	            		        +  u.toExternalForm()
-	            		        + "<"
-	                            );
-	
-}
-
-	
+	            System.out.print  ( "url = >"
+	            		          +  u.toExternalForm()
+	            		          + "<"
+	                              );	
+}	
 }
