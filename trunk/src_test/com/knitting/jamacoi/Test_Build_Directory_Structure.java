835b9790ad8f6500e279645c25527468d0023693
Scott5mith@aol.com <Scott5mith@aol.com@687b8e60-1799-11df-b5b1-cb52af195d9b>
2010-08-02 03:20:20 +0000

    checkpoint changes to Build_Directory_Structure

--- trunk/src_test/com/knitting/jamacoi/Test_Build_Directory_Structure.java
+++ trunk/src_test/com/knitting/jamacoi/Test_Build_Directory_Structure.java
@@ -47,8 +47,30 @@ public class Test_Build_Directory_Structure {
 			                             );
 	bds.create_urls();
 	bds.show_urls  ();
-		
+	bds.check_urls ();
+	bds.create_directories();
+
+	}
+	@Test
+	public        void   test_02()
+	              throws Exception
+	{
+	final  String     app      =  "test/regression/app";
+	final  String     family   =  "appl_amzn_qcom_01";
+	final  String     request  =  "request_01";
 	
-		
+	       Workspace  ws       =   new  Workspace();
+	
+	Build_Directory_Structure  bds = new
+	Build_Directory_Structure            ( ws.get_Workspace() 
+			                             , app
+			                             , family
+			                             , request
+			                             );
+	bds.create_urls();
+	bds.show_urls  ();
+	bds.check_urls ();
+	bds.create_directories();
+
 	}
 }
