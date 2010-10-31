4d85c9bc8f8183ad2f0a8895ebe976cb6e80bc07
Scott5mith@aol.com <Scott5mith@aol.com@687b8e60-1799-11df-b5b1-cb52af195d9b>
2010-10-31 21:57:00 +0000

    initial source load of Directory_Triples class

---
+++ trunk/src/com/knitting/jamacoi/Directory_Triples.java
@@ -0,0 +1,87 @@
+package com.knitting.jamacoi;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Directory_Triples {
+final static String Rel_Base_Dir = "Rel_Base_Dir";	
+	
+private String              Key_Rel_Base_Dir;
+private Map<String, String> Key_Parent;
+
+private Map<String, String> Key_Subdir;
+
+private      Directory_Triples(){;}    //      to prevent this constructor's use.
+public       Directory_Triples( final  String  Subdir
+		                      )
+{
+final String     Base_Dir   =        "Base_Dir";
+
+	         Key_Parent = new     LinkedHashMap<String, String>();
+	         Key_Subdir = new     LinkedHashMap<String, String>();
+	         
+	         Key_Parent . put     ( Base_Dir,   Rel_Base_Dir  );
+	         Key_Subdir . put     ( Base_Dir,   Subdir        );
+	         set_Key_Rel_Base_Dir ( Base_Dir                  );
+}
+protected    void   set_Key_Rel_Base_Dir ( final  String  Key )
+{
+	                    Key_Rel_Base_Dir = Key;
+}
+protected    String get_Key_Rel_Base_Dir ( final  String  Key )
+{
+return	                Key_Rel_Base_Dir;
+}
+public       boolean  Put        ( final  String  Key
+		                         , final  String  Parent
+		                         , final  String  Subdir
+		                         )
+{
+if  (   
+	  (	Key_Subdir.containsKey( Key ) )
+	  ||
+	  ( Key == Rel_Base_Dir           )
+	)
+    {
+	  return false;
+    }
+else
+    {
+	  Key_Parent . put ( Key,  Parent );
+	  Key_Subdir . put ( Key,  Subdir );
+	
+      return true;
+    }
+}
+public boolean  containsKey( final  String  Key )
+{
+	   return   Key_Subdir . containsKey (  Key );
+}
+public boolean  containsParent ( final  String  Parent )
+{
+	   return   (
+			       (Key_Subdir . containsKey    ( Parent ) )
+			       &&
+			       (Key_Parent . containsValue  ( Parent ) )
+	            );
+}
+public ArrayList list_Keys()
+{
+	   ArrayList<String> al   = new ArrayList();
+	   
+	   Set               es   =     Key_Subdir.keySet(); 
+	   Iterator          iter =     es        .iterator();
+	   
+	   while ( iter.hasNext() )
+	         {
+		       al.add( (String) iter.next() );
+	         }
+	   
+	   return            al;
+}
+
+
+}
