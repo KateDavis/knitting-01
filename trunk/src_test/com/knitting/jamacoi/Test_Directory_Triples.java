22aaf536e5d76e2bea39bfb4bf45dab6d2d20e3d
Scott5mith@aol.com <Scott5mith@aol.com@687b8e60-1799-11df-b5b1-cb52af195d9b>
2010-11-01 03:02:08 +0000

    Added tests to Test_Directory_Triples

--- trunk/src_test/com/knitting/jamacoi/Test_Directory_Triples.java
+++ trunk/src_test/com/knitting/jamacoi/Test_Directory_Triples.java
@@ -2,7 +2,12 @@ package com.knitting.jamacoi;
 
 
 import java.util.ArrayList;
+import java.util.Formatter;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -47,5 +52,101 @@ public class Test_Directory_Triples {
 			    		          + "<"
 			    		          );
 		      }
+		        System.out.println( "");
+	}
+	@Test
+	public void Test_List_Subdirs()
+	{
+		LinkedHashMap  <String, String>  lhm  =  dt  . get_Map_Key_Subdir();
+		Set      <Entry<String, String>> es   =  lhm . entrySet();
+		Map.Entry      <String, String>  me;
+		Iterator <Entry<String, String>> iter =  es  . iterator();
+		
+		while ( iter.hasNext() )
+		      {
+			            me      =  iter.next();
+		        String  Key     =  me  .getKey();
+		        String  Subdir  =  me  .getValue();
+		        
+		        Formatter line  =  new Formatter();
+		        
+		        line .format    ( "key = %-15s  Subdir=>%s<"
+		        		        ,  ">" + Key + "<"
+		        		        ,  Subdir
+		        		        );
+		        System.out.println ( line );
+		      }
+		        System.out.println ("");
+	}
+	@Test
+	public void Test_List_Parents()
+	{
+		LinkedHashMap  <String, String>  lhm  =  dt  . get_Map_Key_Parent();
+		Set      <Entry<String, String>> es   =  lhm . entrySet();
+		Map.Entry      <String, String>  me;
+		Iterator <Entry<String, String>> iter =  es  . iterator();
+		
+		while ( iter.hasNext() )
+		      {
+			            me      =  iter.next();
+		        String  Key     =  me  .getKey();
+		        String  Subdir  =  me  .getValue();
+		        
+		        Formatter line  =  new Formatter();
+		        
+		        line .format    ( "key = %-15s  Parent=>%s<"
+		        		        ,  ">" + Key + "<"
+		        		        ,  Subdir
+		        		        );
+		        System.out.println ( line );
+		      }
+		        System.out.println ("");
+	}	
+	@Test
+	public void Test_Add_Subdir_1()
+	{
+		   String  Key    = "request";
+		   String  Parent = "Base_Dir";
+		   String  Subdir = "appl_amzn_qcom";
+		   dt.put( Key,  Parent,  Subdir );
+		   
+		   System.out.println ("");
+		   System.out.println ("Test_Add_Subdir_1 -------------------------------");
+		   System.out.println ("");
+		   Test_List_Keys     ();
+		   Test_List_Subdirs  ();
+		   Test_List_Parents  ();
+		
+	}
+	@Test
+	public void Test_Add_Subdir_2()
+	{
+		   String  Key    = "details";
+		   String  Parent = "Base_Dir";
+		   String  Subdir = "report_details";
+		   dt.put( Key,  Parent,  Subdir );
+		   
+		           Key    = "summary";
+		           Parent = "Base_Dir";
+		           Subdir = "report_summary";
+		   dt.put( Key,  Parent,  Subdir );
+		   
+                   Key    = "auto";
+                   Parent = "request";
+                   Subdir = "correlations_auto";
+           dt.put( Key,  Parent,  Subdir );
+           
+                   Key    = "partial";
+                   Parent = "request";
+                   Subdir = "correlations_partial";
+           dt.put( Key,  Parent,  Subdir );
+           
+           System.out.println ("");
+		   System.out.println ("Test_Add_Subdir_2 -------------------------------");
+		   System.out.println ("");
+		   Test_List_Keys     ();
+		   Test_List_Subdirs  ();
+		   Test_List_Parents  ();
+		
 	}
 }
