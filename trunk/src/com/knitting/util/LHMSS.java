72514a744ce56a121cb0ba6146612db399c501a9
Scott5mith@aol.com <Scott5mith@aol.com@687b8e60-1799-11df-b5b1-cb52af195d9b>
2010-11-11 05:24:36 +0000

    Initial source load

---
+++ trunk/src/com/knitting/util/LHMSS.java
@@ -0,0 +1,51 @@
+package com.knitting.util;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class             LHMSS 
+       implements        Cloneable
+{
+	
+	   private  LinkedHashMap<String, String>  lhm;
+
+
+public  LHMSS()
+{
+	    lhm  =  new  LinkedHashMap<String, String>();
+}
+public  LHMSS(  final  int  size )
+{
+	    lhm  =  new  LinkedHashMap<String, String>( size );
+}
+public  LHMSS   clone()
+{
+        return  (  (LHMSS) lhm.clone()  );	
+}
+public  String  put( final  String  Key
+		           , final  String  Value
+		           )
+{
+        return     ( lhm.put( Key, Value ) );
+}
+public  boolean  containsKey( final  String  Key )
+{
+	    return     ( lhm.containsKey       ( Key ) );
+}
+public  boolean  containsValue( final  String  Value )
+{
+	    return     ( lhm.containsValue       ( Value ) );
+}
+public  void     putAll( final  LHMSS  lhm_in )
+{
+	             lhm.putAll( (Map<? extends String, ? extends String>) lhm_in );
+}
+public  LinkedHashMap<String, String>    get()
+{
+	    return   lhm;
+}
+public  String    get( final  String  Key )
+{
+	    return    lhm.get           ( Key );
+}
+}
