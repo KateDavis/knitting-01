2706e6063dc80127afbc586ca60db9916862db0c
Scott5mith@aol.com <Scott5mith@aol.com@687b8e60-1799-11df-b5b1-cb52af195d9b>
2010-11-14 20:11:50 +0000

    deleted commented out code.

--- trunk/src/com/knitting/jamacoi/Directory_Triples.java
+++ trunk/src/com/knitting/jamacoi/Directory_Triples.java
@@ -13,45 +13,36 @@ public class Directory_Triples
 final static String Rel_Base_Dir = "Rel_Base_Dir";	
 	
   private String                          Key_Rel_Base_Dir;
-//private LinkedHashMap<String, String>   Key_Parent;
   private LHMSS                           Key_Parent;
-//private LinkedHashMap<String, String>   Key_Subdir;
   private LHMSS                           Key_Subdir;
 
-private      Directory_Triples(){;}    //      to prevent this constructor's use.
-public       Directory_Triples( final  String  Subdir
-		                      )
+private       Directory_Triples(){;}    //      to prevent this constructor's use.
+public        Directory_Triples( final  String  Subdir
+		                       )
 {
-final String     Base_Dir   =        "Base_Dir";
-
-//	         Key_Parent = new     LinkedHashMap<String, String>();
-             Key_Parent = new     LHMSS();
-//	         Key_Subdir = new     LinkedHashMap<String, String>();
-	         Key_Subdir = new     LHMSS();
+final String  Base_Dir   =        "Base_Dir";
+              Key_Parent = new     LHMSS();
+	          Key_Subdir = new     LHMSS();
 	         
-	         Key_Parent . put     ( Base_Dir,   Rel_Base_Dir  );
-	         Key_Subdir . put     ( Base_Dir,   Subdir        );
-	         set_Key_Rel_Base_Dir ( Base_Dir                  );
+	          Key_Parent . put     ( Base_Dir,   Rel_Base_Dir  );
+	          Key_Subdir . put     ( Base_Dir,   Subdir        );
+	          set_Key_Rel_Base_Dir ( Base_Dir                  );
 }
-//private    Directory_Triples    ( LinkedHashMap<String,  String>  Key_Parent_Old 
-//		                          , LinkedHashMap<String,  String>  Key_Subdir_Old
-  private    Directory_Triples    ( LHMSS                           Key_Parent_Old 
-		                          , LHMSS                           Key_Subdir_Old
-		                          , String                          Key_Rel_Base_Dir
-		                          )
+  private     Directory_Triples    ( LHMSS                           Key_Parent_Old 
+		                           , LHMSS                           Key_Subdir_Old
+		                           , String                          Key_Rel_Base_Dir
+		                           )
 {	         
-//	         Key_Parent  =         (LinkedHashMap<String,  String>) Key_Parent_Old.clone();
-             Key_Parent  =         (LHMSS                         ) Key_Parent_Old.clone();
-//	         Key_Subdir  =         (LinkedHashMap<String,  String>) Key_Subdir_Old.clone();
-	         Key_Subdir  =         (LHMSS                         ) Key_Subdir_Old.clone();
+              Key_Parent  =         (LHMSS                         ) Key_Parent_Old.clone();
+	          Key_Subdir  =         (LHMSS                         ) Key_Subdir_Old.clone();
 }
-public       Directory_Triples    clone( )
+public        Directory_Triples    clone( )
 {
-	         Directory_Triples    clone  =  new Directory_Triples( this.Key_Parent
-	        		                                             , this.Key_Subdir
-	        		                                             , "test"
-	        		                                             );
-	         return               clone;
+	          Directory_Triples    clone  =  new Directory_Triples( this.Key_Parent
+	        	  	                                              , this.Key_Subdir
+	        		                                              , "test"
+	        		                                              );
+	          return               clone;
 }
 
 protected    void   set_Key_Rel_Base_Dir ( final  String  Key )
@@ -108,10 +99,9 @@ public  boolean contains_Key( final  String  Key)
 	    	   return false;
 	         }
 }
-//protected  LinkedHashMap< String,  String > get_Key_Subdir()
-  protected  LHMSS get_Key_Subdir()
+protected  LHMSS get_Key_Subdir()
 {
-return                                          Key_Subdir;
+return               Key_Subdir;
 }
 public boolean  containsKey( final  String  Key )
 {
@@ -125,58 +115,41 @@ public boolean  containsParent ( final  String  Parent )
 			       (Key_Parent . containsValue  ( Parent ) )
 	            );
 }
-public ArrayList<String> list_Keys()
+public  ArrayList<String> list_Keys()
 {
-	   ArrayList<String>  al   = new ArrayList<String>  ( Key_Subdir.size() );
-	   Set      <String>  es   =     Key_Subdir.keySet  (); 
-	   Iterator <String>  iter =     es        .iterator();
+	    ArrayList<String>  al   = new ArrayList<String>    ( Key_Subdir . size    () );
+	    Set      <String>  es   =                            Key_Subdir . keySet  (); 
+	    Iterator <String>  iter =                            es         . iterator();
 	   
-	   while ( iter.hasNext() )
-	         {
-		       al.add( (String) iter.next() );
-	         }
-	   
-	   return            al;
+	    while ( iter.hasNext() )
+	          {
+		        al.add( (String) iter.next() );
+	          }
+	    return            al;
 }
-//public       LinkedHashMap<String, String>  get_Map_Key_Subdir()
-//{
-//	    final  LinkedHashMap<String, String>  ks  =  new  LinkedHashMap<String, String> ( Key_Subdir );
-//	    return                                ks;
-//}
 public            LHMSS  get_Map_Key_Subdir()
 {
 	    return    Key_Subdir;              
 }
-
-//public       LinkedHashMap<String, String>  get_Map_Key_Parent()
-//{
-// 	    final  LinkedHashMap<String, String>  kp  =  new  LinkedHashMap<String, String> ( Key_Parent );
-//	    return                                kp;
-//}
-
-
 public            LHMSS  get_Map_Key_Parent()
 {
 	    return    Key_Parent;              
 }
 public  boolean  remove_Subdir ( final String  Key )
 {
-	    if    ( Key_Subdir.containsKey( Key ) )
-	          { 
-	    	    rebuild_without  ( Key );
-	    	    return true;
-	          }
-	    else  {
-	    	    return false;
-	          }
+	    if     ( Key_Subdir.containsKey      ( Key ) )
+	           { 
+	    	     rebuild_without             ( Key );
+	    	     return true;
+	           }
+	    else   {
+	    	     return false;
+	           }
 }
 protected  void  rebuild_without ( final  String  Key_In )
 {
-//	        LinkedHashMap <String, String>  Key_Parent_New  =  new  LinkedHashMap <String, String>( Key_Parent.size() );
             LHMSS                           Key_Parent_New  =  new  LHMSS                         ( Key_Parent.size() );
-//	        LinkedHashMap <String, String>  Key_Subdir_New  =  new  LinkedHashMap <String, String>( Key_Subdir.size() );
 	        LHMSS                           Key_Subdir_New  =  new  LHMSS                         ( Key_Subdir.size() );
-//	        LinkedHashMap <String, String>  Excluded_List   =  new  LinkedHashMap <String, String>( Key_Subdir.size() );
 	        LHMSS                           Excluded_List   =  new  LHMSS                         ( Key_Subdir.size() );
 	        
 	        Excluded_List.put( Key_In, "exclude");
