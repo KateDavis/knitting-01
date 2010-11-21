fd454f9ceb876c92844f8e3e501f970f48109eb3
Scott5mith@aol.com <Scott5mith@aol.com@687b8e60-1799-11df-b5b1-cb52af195d9b>
2010-11-21 22:44:07 +0000

    Added Directory_Manager class together with some associated changes in other classes

---
+++ trunk/src_test/com/knitting/jamacoi/LHMESURL.java
@@ -0,0 +1,10 @@
+package com.knitting.jamacoi;
+
+import java.net.URL;
+import java.util.LinkedHashMap;
+
+public   class    LHMESURL 
+         extends  LinkedHashMap < String, URL >
+{
+private  static   final   long    serialVersionUID  =  6413126170207310838L;
+}
