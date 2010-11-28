2c94a317eca27a1764803002b2482b5ecbdf7a2c
Scott5mith <Scott5mith@aol.com>
2010-11-28 20:45:18 +0000

    deleted commented-out source and unreferenced methods

--- src_test/com/knitting/jamacoi/Test_Directory_Triples.java
+++ src_test/com/knitting/jamacoi/Test_Directory_Triples.java
@@ -2,18 +2,13 @@ package com.knitting.jamacoi;
 
 
 import java.util.ArrayList;
-import java.util.Formatter;
 import java.util.Iterator;
-import java.util.Map;
-import java.util.Map.Entry;
 
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-import com.knitting.util.LHMESS;
 
 public class Test_Directory_Triples {
 	Directory_Triples dt;
@@ -447,39 +442,5 @@ public class Test_Directory_Triples {
 			    		          );
 		      }
 		        System.out.println( "");
-	}
-/****************************************************************************	
-	public  void  List_Parents( final  Directory_Triples  dt )
-	{	
-		    LHMESS              lhm    =  dt   . get_Map_Key_Parent();		
-		    list_Values       ( lhm,  "Parent" );
-	}
-	public  void  List_Subdirs( final  Directory_Triples  dt )
-	{	
-		    LHMESS              lhm    =  dt   . get_Map_Key_Subdir();		
-		    list_Values       ( lhm,  "Subdir" );
-	}
-***************************************************************************/	
-	private void list_Values  ( final  LHMESS  lhm
-			                  , final  String  title
-			                  )
-	{
-		for ( Iterator <Entry<String, String>>   iter    =  lhm  . entrySet ()
-				                                                 . iterator ();
-			                                     iter            . hasNext  (); 	
-			)		
-		    {
-			   Map.Entry  <String, String>       me      =  iter . next     ();
-		       String                            Key     =  me   . getKey   ();
-		       String                            Value   =  me   . getValue ();
-		       Formatter                         line    =  new    Formatter();
-		       line .format       ( "key = %-15s  %s=>%s<"
-		        		          ,  ">" + Key + "<"
-		        		          ,  title
-		        		          ,  Value
-		        		          );
-		       System.out.println ( line );
-		    }
-		       System.out.println ("");
-	}		
+	}	
 }
