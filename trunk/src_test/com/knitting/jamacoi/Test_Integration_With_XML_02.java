c4caebc7632f4fb2fdb04f153404012d289ffbc0
Scott5mith@aol.com <Scott5mith@aol.com@687b8e60-1799-11df-b5b1-cb52af195d9b>
2010-12-03 18:57:04 +0000

    addition refactoring changes

--- trunk/src_test/com/knitting/jamacoi/Test_Integration_With_XML_02.java
+++ trunk/src_test/com/knitting/jamacoi/Test_Integration_With_XML_02.java
@@ -11,21 +11,11 @@ import org.junit.Test;
 
 public class Test_Integration_With_XML_02 {
 	
-	static String      Dir__Out_Base         =  "knitting-01/src_test/com/knitting/out_base";
-	static String      Dir__Out_Family       =  "aapl_amzn_qcom";
-	static String      Dir__Out_Request      =  "request_001";
 	
-	static String      Dir__Exp_Act          =  "knitting-01/src_test/com/knitting/jamacoi";
-	static String      Dir__Exp              =  "expected";
-	static String      Dir__Act              =  "actual";
-	
-	static String      Dir__Src_Res          = "knitting-01/src_resources";
-	static String      Dir__Resources        = "knitting-01/src_resources/com/knitting/resources";
-	
-	Directory_Triples  dir_t01;
 	Directory_Triples  dir_t02;
 	Directory_Triples  dir_t03;
 	
+	;
 	Directory_Manager  dir_m01;
 	Directory_Manager  dir_m02;
 	Directory_Manager  dir_m03;
@@ -41,32 +31,40 @@ public class Test_Integration_With_XML_02 {
 	@Before
 	public void setUp() throws Exception {
 		
-    create_dir_t01_and_dir_m01();
+    dir_m01  =  create_dir_m01();
     create_dir_t02_and_dir_m02();
     create_dir_t03_and_dir_m03();
     
 	}
-	private  void        create_dir_t01_and_dir_m01() 
-	         throws      MalformedURLException 
+	private  Directory_Manager     create_dir_m01() 
+	         throws                MalformedURLException 
 	{
-		     dir_t01     =  new Directory_Triples ( "Dir__Out_Base"
-		                                          ,  Dir__Out_Base
-				                                  );	
-		     dir_t01.put ( "Dir__Out_Family"
-				         , "Dir__Out_Base"
-				         ,  Dir__Out_Family
-				         );
+		     String      Dir__Out_Base         =  "knitting-01/src_test/com/knitting/out_base";
+		     String      Dir__Out_Family       =  "aapl_amzn_qcom";
+		     String      Dir__Out_Request      =  "request_001";	
+		     
+		     Directory_Triples     dir_t01     =  new Directory_Triples ( "Dir__Out_Base"
+		                                                                ,  Dir__Out_Base
+				                                                        );	
+		                           dir_t01.put ( "Dir__Out_Family"
+				                               , "Dir__Out_Base"
+				                               ,  Dir__Out_Family
+				                               );
 		
-		     dir_t01.put ( "Dir__Out_Request"
-		                 , "Dir__Out_Family"
-		                 ,  Dir__Out_Request
-		                 );
+		                           dir_t01.put ( "Dir__Out_Request"
+		                                       , "Dir__Out_Family"
+		                                       ,  Dir__Out_Request
+		                                       );
 		
-		     dir_m01     =  new  Directory_Manager ( dir_t01 );
+		     return     new  Directory_Manager ( dir_t01 );
 	}
 	private  void    create_dir_t02_and_dir_m02() 
 	         throws  MalformedURLException 
 	{
+		     String      Dir__Exp_Act          =  "knitting-01/src_test/com/knitting/jamacoi";
+		     String      Dir__Exp              =  "expected";
+		     String      Dir__Act              =  "actual";
+		
 	         dir_t02     =  new  Directory_Triples ( "Dir__Exp_Act"
 				                                   ,  Dir__Exp_Act
 				                                   );
@@ -86,6 +84,9 @@ public class Test_Integration_With_XML_02 {
 	private  void        create_dir_t03_and_dir_m03() 
 	         throws      MalformedURLException 
 	{
+		     String      Dir__Src_Res          = "knitting-01/src_resources";
+		     String      Dir__Resources        = "knitting-01/src_resources/com/knitting/resources";	
+		     
 	   	     dir_t03     =  new Directory_Triples  ( "Dir__Src_Res"
 		                                           ,  Dir__Src_Res
 		                                           );	
@@ -101,6 +102,7 @@ public class Test_Integration_With_XML_02 {
 	@After
 	public void tearDown() throws Exception {
 	}
+	/*************************************************************
 	@Test
 	public  void    test_List_Parents()
 	{
@@ -111,6 +113,7 @@ public class Test_Integration_With_XML_02 {
 	{
 		         dir_t01.List_Subdirs();
 	}
+	*************************************************************/
 	@Test
 	public  void    test_workspace()
 	        throws  java.net.MalformedURLException 
