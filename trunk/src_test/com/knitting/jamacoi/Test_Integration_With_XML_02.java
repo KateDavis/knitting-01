9c3c29ab5a9787afd4daf52e7a7b149370714eaf
Scott5mith@aol.com <Scott5mith@aol.com@687b8e60-1799-11df-b5b1-cb52af195d9b>
2010-12-04 04:21:56 +0000

    some more refactoring

--- trunk/src_test/com/knitting/jamacoi/Test_Integration_With_XML_02.java
+++ trunk/src_test/com/knitting/jamacoi/Test_Integration_With_XML_02.java
@@ -29,91 +29,78 @@ public class Test_Integration_With_XML_02 {
 	}
 
 	@Before
-	public void setUp() throws Exception {
-		
-    dir_m01  =  create_dir_m01();
-    create_dir_t02_and_dir_m02();
-    create_dir_t03_and_dir_m03();
-    
+	public   void        setUp() 
+	         throws      Exception 
+	{	
+             dir_m01  =  create_dir_m01();
+             dir_m02  =  create_dir_m02();
+             dir_m03  =  create_dir_m03();
 	}
 	private  Directory_Manager     create_dir_m01() 
-	         throws                MalformedURLException 
-	{
-		     String      Dir__Out_Base         =  "knitting-01/src_test/com/knitting/out_base";
-		     String      Dir__Out_Family       =  "aapl_amzn_qcom";
-		     String      Dir__Out_Request      =  "request_001";	
-		     
-		     Directory_Triples     dir_t01     =  new Directory_Triples ( "Dir__Out_Base"
-		                                                                ,  Dir__Out_Base
-				                                                        );	
-		                           dir_t01.put ( "Dir__Out_Family"
-				                               , "Dir__Out_Base"
-				                               ,  Dir__Out_Family
-				                               );
-		
-		                           dir_t01.put ( "Dir__Out_Request"
-		                                       , "Dir__Out_Family"
-		                                       ,  Dir__Out_Request
-		                                       );
-		
-		     return     new  Directory_Manager ( dir_t01 );
-	}
-	private  void    create_dir_t02_and_dir_m02() 
-	         throws  MalformedURLException 
-	{
-		     String      Dir__Exp_Act          =  "knitting-01/src_test/com/knitting/jamacoi";
-		     String      Dir__Exp              =  "expected";
-		     String      Dir__Act              =  "actual";
-		
-	         dir_t02     =  new  Directory_Triples ( "Dir__Exp_Act"
-				                                   ,  Dir__Exp_Act
-				                                   );
-		
-	        dir_t02.put ( "Dir__Exp"
-                        , "Dir__Exp_Act"
-				        ,  Dir__Exp
-				        );
-		
-		    dir_t02.put ( "Dir__Act"
-			   	        , "Dir__Exp_Act" 
-				        ,  Dir__Act
-				        );
-		
-		    dir_m02     =  new  Directory_Manager ( dir_t02 );
-	}
-	private  void        create_dir_t03_and_dir_m03() 
-	         throws      MalformedURLException 
-	{
-		     String      Dir__Src_Res          = "knitting-01/src_resources";
-		     String      Dir__Resources        = "knitting-01/src_resources/com/knitting/resources";	
-		     
-	   	     dir_t03     =  new Directory_Triples  ( "Dir__Src_Res"
-		                                           ,  Dir__Src_Res
-		                                           );	
-   
-             dir_t03.put ( "Dir__Resources"
-			             , "Dir__Src_Res"
-			             ,  Dir__Resources
-			             );
-		
-	         dir_m03     =  new  Directory_Manager ( dir_t03 );
-	}
+             throws                MalformedURLException 
+    {
+             String                Dir__Out_Base         =  "knitting-01/src_test/com/knitting/out_base";
+             String                Dir__Out_Family       =  "aapl_amzn_qcom";
+             String                Dir__Out_Request      =  "request_001";	
 
+             Directory_Triples     dt     =  new Directory_Triples ( "Dir__Out_Base"
+                                                                   ,  Dir__Out_Base
+                                                                   );	
+                                   dt.put ( "Dir__Out_Family"
+                                          , "Dir__Out_Base"
+                                          ,  Dir__Out_Family
+                                          );
+
+                                   dt.put ( "Dir__Out_Request"
+                                          , "Dir__Out_Family"
+                                          ,  Dir__Out_Request
+                                          );
+
+             return                new    Directory_Manager ( dt );
+    }
+    private  Directory_Manager     create_dir_m02() 
+             throws                MalformedURLException 
+    {
+             String                Dir__Exp_Act          =  "knitting-01/src_test/com/knitting/jamacoi";
+             String                Dir__Exp              =  "expected";
+             String                Dir__Act              =  "actual";
+
+             Directory_Triples     dt     =  new  Directory_Triples ( "Dir__Exp_Act"
+                                                                    ,  Dir__Exp_Act
+                                                                    );
+                                   dt.put ( "Dir__Exp"
+                                          , "Dir__Exp_Act"
+                                          ,  Dir__Exp
+                                          );
+
+                                   dt.put ( "Dir__Act"
+	                                      , "Dir__Exp_Act" 
+                                          ,  Dir__Act
+                                          );
+
+             return                new    Directory_Manager ( dt );
+    }
+private  Directory_Manager     create_dir_m03() 
+         throws                MalformedURLException 
+{
+         String                Dir__Src_Res          = "knitting-01/src_resources";
+         String                Dir__Resources        = "com/knitting/resources";	
+
+         Directory_Triples     dt     =  new Directory_Triples  ( "Dir__Src_Res"
+                                                                ,  Dir__Src_Res
+                                                                );	
+                               dt.put ( "Dir__Resources"
+                                      , "Dir__Src_Res"
+                                      ,  Dir__Resources
+                                      );
+
+         return                new    Directory_Manager ( dt );
+}
 	@After
-	public void tearDown() throws Exception {
-	}
-	/*************************************************************
-	@Test
-	public  void    test_List_Parents()
+	public  void    tearDown() 
+	        throws  Exception 
 	{
-		         dir_t01.List_Parents();
 	}
-	@Test
-	public  void    test_List_Subdirs()
-	{
-		         dir_t01.List_Subdirs();
-	}
-	*************************************************************/
 	@Test
 	public  void    test_workspace()
 	        throws  java.net.MalformedURLException 
