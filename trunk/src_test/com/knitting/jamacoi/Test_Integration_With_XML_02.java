5372397cb092ed7a035b96f525ea52201f7f2d91
Scott5mith@aol.com <Scott5mith@aol.com@687b8e60-1799-11df-b5b1-cb52af195d9b>
2010-12-04 22:15:40 +0000

    Added another Directory_Manager object, and found a missing sub_directory that belonged to an existing Directory_Manager

--- trunk/src_test/com/knitting/jamacoi/Test_Integration_With_XML_02.java
+++ trunk/src_test/com/knitting/jamacoi/Test_Integration_With_XML_02.java
@@ -17,8 +17,9 @@ public class Test_Integration_With_XML_02 {
 	
 	;
 	Directory_Manager  dir_m01;
-	Directory_Manager  dir_m02;
-	Directory_Manager  dir_m03;
+	Directory_Manager  dir_m02_exp_act;
+	Directory_Manager  dir_m03_resources;
+	Directory_Manager  dir_m04_data_sources;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -33,8 +34,9 @@ public class Test_Integration_With_XML_02 {
 	         throws      Exception 
 	{	
              dir_m01  =  create_dir_m01();
-             dir_m02  =  create_dir_m02();
-             dir_m03  =  create_dir_m03();
+             dir_m02_exp_act       =  create_dir_m02_exp_act();
+             dir_m03_resources     =  create_dir_m03_resources();
+             dir_m04_data_sources  =  create_dir_m04_data_sources();
 	}
 	private  Directory_Manager     create_dir_m01() 
              throws                MalformedURLException 
@@ -58,7 +60,7 @@ public class Test_Integration_With_XML_02 {
 
              return                new    Directory_Manager ( dt );
     }
-    private  Directory_Manager     create_dir_m02() 
+    private  Directory_Manager     create_dir_m02_exp_act() 
              throws                MalformedURLException 
     {
              String                Dir__Exp_Act          =  "knitting-01/src_test/com/knitting/jamacoi";
@@ -80,22 +82,39 @@ public class Test_Integration_With_XML_02 {
 
              return                new    Directory_Manager ( dt );
     }
-private  Directory_Manager     create_dir_m03() 
-         throws                MalformedURLException 
-{
-         String                Dir__Src_Res          = "knitting-01/src_resources";
-         String                Dir__Resources        = "com/knitting/resources";	
+    private  Directory_Manager     create_dir_m03_resources() 
+             throws                MalformedURLException 
+    {
+             String                Dir__Src_Res          = "knitting-01/src_resources";
+             String                Dir__Resources        = "com/knitting/resources";
+             String                Dir__Config           = "configuration";
 
-         Directory_Triples     dt     =  new Directory_Triples  ( "Dir__Src_Res"
-                                                                ,  Dir__Src_Res
-                                                                );	
-                               dt.put ( "Dir__Resources"
-                                      , "Dir__Src_Res"
-                                      ,  Dir__Resources
-                                      );
+             Directory_Triples     dt     =  new Directory_Triples  ( "Dir__Src_Res"
+                                                                    ,  Dir__Src_Res
+                                                                    );	
+                                   dt.put ( "Dir__Resources"
+                                          , "Dir__Src_Res"
+                                          ,  Dir__Resources
+                                          );
 
-         return                new    Directory_Manager ( dt );
-}
+                                   dt.put ( "Dir__Config"
+                                          , "Dir__Resources"
+                                          ,  Dir__Config
+                                          );
+                                   
+             return                new    Directory_Manager ( dt );
+    }
+    private  Directory_Manager     create_dir_m04_data_sources() 
+             throws                MalformedURLException 
+    {
+             String                Dir__Prefix_In        = "knitting-01/src_test/com/knitting/datasource";
+
+             Directory_Triples     dt     =  new Directory_Triples  ( "Dir__Prefix_In"
+                                                                    ,  Dir__Prefix_In
+                                                                    );	
+
+             return                new    Directory_Manager ( dt );
+    }
 	@After
 	public  void    tearDown() 
 	        throws  Exception 
@@ -131,13 +150,19 @@ private  Directory_Manager     create_dir_m03()
 	@Test
 	public  void  test_expected_actual_directories()
 	{
-		    dir_m02.check_urls();
+		    dir_m02_exp_act.check_urls();
 	        System.out.println("");
 	}
 	@Test
 	public  void  test_resources()
 	{
-		    dir_m03.check_urls();
+		    dir_m03_resources.check_urls();
+	        System.out.println("");
+	}
+	@Test
+	public  void  test_resources_data()
+	{
+		    dir_m04_data_sources.check_urls();
 	        System.out.println("");
 	}
 }
